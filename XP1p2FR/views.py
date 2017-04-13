75b44531970a9fdef54b7c8e6b68883da78779de
jules-selles <jules.selles@gmail.com>
2017-04-13 14:33:37 +0200

    update lameta

--- XP1p2FR/views.py
+++ XP1p2FR/views.py
@@ -371,6 +371,7 @@ class Catch_Results(Page):
         oProfit = []
         oID = []
         oData =[]
+        predProfit_round = []
         IndHarvestEuros = []
         IndpredEuros = []
 
@@ -378,11 +379,13 @@ class Catch_Results(Page):
         for p in self.player.in_all_rounds():
             catch_round.append(p.catch_choice)
             profit_round.append(p.profit)
-            totalIndCatch = sum(catch_round)
-            totalIndProfit = round(sum(profit_round), 1)
+            totalIndCatch   = sum(catch_round)
+            totalIndProfit  = round(sum(profit_round), 1)
             IndHarvestEuros = round(sum(profit_round) * Constants.convertionCurrency, 1)
+            predProfit_round.append(p.predProfit)
+            IndpredEuros    = round(sum(predProfit_round), 1)
 
-        IndpredEuros = self.participant.payoff - round(IndHarvestEuros,1)
+        #IndpredEuros = self.participant.payoff - round(IndHarvestEuros,1)
 
         # others cacth & profit per player
         for p in self.player.get_others_in_group():
@@ -489,124 +492,26 @@ class Catch_Results(Page):
                 'colorBlim_label': colorBlim_label
         }
 
-##-------------------------------
-class ScientificAdvice(Page):
-
-    timeout_seconds = 60
-
-    def is_displayed(self):
-        return self.group.b_round > 0 or self.group.end is False
-
-    ##-------------------------------
-    ## variables for template
-    def vars_for_template(self):
-
-        ## create area range series for projection uncertainty on biomass plot
-        ## projection uncertainty
-        proj = self.group.projection()
-        proj_un = self.group.projUncertainty()
-
-        j = -1
-        UNarea = []
-        seq = list(range(len(proj) + 1))
-
-        # simplify nested list
-        for i in range(1, 2):
-            unArea = sum(proj_un, [])
-
-        for row in unArea[0]:
-            j = j + 1
-            UNarea.append([[seq[j]] + row])
-
-        UNarea = sum(UNarea, [])
-
-        ## color setting
-        if self.session.config['treatment'] == 'T1':
-            colorBlim = "rgba(68, 170, 213, 0)"
-            colorBlim_label='rgba(68, 170, 213, 0)'
-            colorBlim_range = 'rgba(68, 170, 213, 0)'
-            colorBlim_range_label='rgba(68, 170, 213, 0)'
-        elif self.session.config['treatment'] == 'T2':
-            colorBlim ="red"
-            colorBlim_label='gray'
-            colorBlim_range = 'rgba(68, 170, 213, 0)'
-            colorBlim_range_label='rgba(68, 170, 213, 0)'
-        elif self.session.config['treatment'] == 'T3':
-            colorBlim ="rgba(68, 170, 213, 0)"
-            colorBlim_label='rgba(68, 170, 213, 0)'
-            colorBlim_range= 'rgba(213, 70, 150, 0.2)'
-            colorBlim_range_label = 'gray'
-
-        ## Filling the data for graph
-        ## Biomass estimation + projection under statu quo (same harvest level)
-
-        data = {'Biomass': self.group.b_round,
-                'Bmsy': Constants.Bmsy,
-                'Blim':Constants.Blim,
-                'Projection': proj,
-                'UnRange': UNarea,
-                'Bmax':self.group.bmax_round,
-                'Bmin':self.group.bmin_round,
-                'Blim_min':self.group.Blim_min,
-                'Blim_max': self.group.Blim_max,
-                'years' : Constants.xp_years,
-                'colorBlim':colorBlim,
-                'colorBlim_range':colorBlim_range,
-                'colorBlim_range_label':colorBlim_range_label,
-                'colorBlim_label':colorBlim_label}
-
-        data['seriesBiomass'] = list()
-        data['seriesBmsy'] = list()
-        data['seriesBlim'] = list()
-        data['seriesProjection'] = list()
-        data['seriesUnRange'] = list()
-        data['seriesBrange'] = list()
-        data['seriesBlim_min'] = list()
-        data['seriesBlim_max'] = list()
-
-        data['seriesProjection'].append({'name': 'Projection',
-                                     'data': proj})
-        data['seriesBiomass'].append({'name': 'Biomass',
-                                  'data': self.group.b_round})
-        # data['seriesBmsy'].append({'name': 'Bmsy',
-        #                          'data': self.group.target})
-        # data['seriesBlim'].append({'name': 'Bmsy',
-        #                          'data': self.group.lim})
-        data['seriesUnRange'].append({'name': 'UnRange',
-                                  'data': UNarea})
-        data['seriesBlim_min'].append({'name': 'Blim_min',
-                                   'data': self.group.Blim_min})
-        data['seriesBlim_max'].append({'name': 'Blim_max',
-                                   'data': self.group.Blim_max})
-
-        data['seriesBiomass'] = safe_json(data['seriesBiomass'])
-        data['seriesBmsy'] = safe_json(data['seriesBmsy'])
-        data['seriesBlim'] = safe_json(data['seriesBlim'])
-        data['seriesProjection'] = safe_json(data['seriesProjection'])
-        data['seriesUnRange'] = safe_json(data['seriesUnRange'])
-        data['seriesBrange'] = safe_json(data['seriesBrange'])
-        data['seriesBlim_min'] = safe_json(data['seriesBlim_min'])
-        data['seriesBlim_max'] = safe_json(data['seriesBlim_max'])
-
-        return data
-
 class End(Page):
 
     timeout_seconds = 60
 
     def vars_for_template(self):
-        c_round =[]
-        p_round =[]
-        totC    =[]
-        totP    =[]
+        c_round    =[]
+        p_round    =[]
+        pred_round =[]
+        totC       =[]
+        totP       =[]
+        totPred    =[]
 
         for p in self.player.in_all_rounds():
             c_round.append(p.catch_choice)
             p_round.append(p.profit)
             totC = sum(c_round)
             totP = sum(p_round)
+            totPred = sum(pred_round)
 
-        euros = totP * Constants.convertionCurrency
+        euros = round(totP * Constants.convertionCurrency,1) + round(totPred,1)
 
         if self.group.b_round <= 0:
             message = message='Vous avez conduit le stock à l effondrement!! '
@@ -656,7 +561,6 @@ page_sequence = [
     Catch_Choice,
     CatchChoice_WaitPage,
     Catch_Results,
-    #ScientificAdvice,
     End,
     Formend
 ]
